8ed64a909eccfc18e834f23d3a7796abd87c55ba
Harry <harry811016@gmail.com>
2020-06-22 13:19:18 +0800

    project init

---
+++ routes/modules/user.js
@@ -0,0 +1,69 @@
+// routes/user.js
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user')
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+// 登入頁面
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+// 註冊頁面
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ email }).then(user => {
+    if (user) {                                       // 檢查 email 是否存在
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {                // 使用者已經註冊過
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => { res.redirect('/') })
+      .catch(err => console.log(err))
+
+  })
+})
+// 登出
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
+})
+
+module.exports = router
