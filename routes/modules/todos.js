b7f32f8069372f294e2b6f30bc478ae1bd70974a
Harry <harry811016@gmail.com>
2020-06-24 14:12:25 +0800

    add CRUD function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,6 +6,18 @@ const db = require('../../models')
 const Todo = db.Todo
 
 // 定義路由
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
@@ -14,6 +26,37 @@ router.get('/:id', (req, res) => {
   })
     .then((todo) => { return res.render('detail', { todo: todo.toJSON() }) })
     .catch((error) => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ id, UserId })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = (isDone === 'on')
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // 匯出路由器
